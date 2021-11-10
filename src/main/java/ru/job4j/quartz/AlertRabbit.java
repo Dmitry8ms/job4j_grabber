cfef86bb991cbafc0258c663334af654848ff2cf
Dmitry8ms <dshatokhin@yandex.ru>
2021-11-10 21:27:25 +0700

    1. Quartz [#175122]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,7 +3,6 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.File;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -12,33 +11,38 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
     public static void main(String[] args) {
-        String propsFile = AlertRabbit.class.getClassLoader().getResource("rabbit.properties")
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(getIntervalFrom("rabbit.properties"))
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    private static int getIntervalFrom(String fileName) {
+        String propsFile = AlertRabbit.class.getClassLoader().getResource(fileName)
                 .getFile();
         Properties prs = LoadProps.getLoadedPropsFrom(propsFile);
         String interval = prs.getProperty("rabbit.interval");
         int intervalInt = 0;
         try {
             intervalInt = Integer.parseInt(interval);
+            if (intervalInt <= 0) {
+                throw new NumberFormatException();
+            }
         } catch (NumberFormatException e) {
             System.out.println("Wrong interval parameter in rabbit.properties");
         }
-        if (intervalInt > 0) {
-            try {
-                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-                scheduler.start();
-                JobDetail job = newJob(Rabbit.class).build();
-                SimpleScheduleBuilder times = simpleSchedule()
-                        .withIntervalInSeconds(intervalInt)
-                        .repeatForever();
-                Trigger trigger = newTrigger()
-                        .startNow()
-                        .withSchedule(times)
-                        .build();
-                scheduler.scheduleJob(job, trigger);
-            } catch (SchedulerException se) {
-                se.printStackTrace();
-            }
-        }
+        return intervalInt;
     }
 
     public static class Rabbit implements Job {
