f7eb48442b9bb4b466145689a565509c56cfca71
Dmitry8ms <dshatokhin@yandex.ru>
2021-11-12 15:25:42 +0700

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,47 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.Map;
+
+import static java.util.Map.entry;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+
+    private static final Map<String, Month> MONTHS = Map.ofEntries(
+            entry("янв", Month.JANUARY), entry("фев", Month.FEBRUARY),
+            entry("мар", Month.MARCH), entry("май", Month.MAY),
+            entry("апр", Month.APRIL), entry("июн", Month.JUNE),
+            entry("июл", Month.JULY), entry("авг", Month.AUGUST),
+            entry("сен", Month.SEPTEMBER), entry("окт", Month.OCTOBER),
+            entry("ноя", Month.NOVEMBER), entry("дек", Month.DECEMBER)
+    );
+
+    private static final Map<String, LocalDate> LOCAL_DATE_MAP = Map.ofEntries(
+            entry("сегодня", LocalDate.now()), entry("вчера", LocalDate.now().minusDays(1))
+    );
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        LocalDate date = null;
+        String[] dateTime = parse.split(",");
+        String dateStr = dateTime[0].trim();
+        String timeStr = dateTime[1].trim();
+        String[] hourSec = timeStr.split(":");
+        LocalTime time = LocalTime.of(Integer.parseInt(
+                hourSec[0].trim()), Integer.parseInt(hourSec[1].trim()));
+        String[] dayMonthYear = dateStr.split(" ");
+        if (dayMonthYear.length == 3) {
+            int day = Integer.parseInt(dayMonthYear[0].trim());
+            Month month = MONTHS.get(dayMonthYear[1].trim());
+            int year = Integer.parseInt("20" + dayMonthYear[2].trim());
+            date = LocalDate.of(year, month, day);
+        } else if (dayMonthYear.length == 1) {
+            date = LOCAL_DATE_MAP.get(dayMonthYear[0].trim());
+        }
+        return LocalDateTime.of(date, time);
+    }
+}
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -1,18 +1,29 @@
 package ru.job4j.html;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.time.format.DateTimeFormatter;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+                "dd.MM.yyyy HH:mm");
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
         for (Element td : row) {
             Element parent = td.parent();
             System.out.println(parent.child(1).attr("href"));
             System.out.println(parent.child(1).text());
-            System.out.println(parent.child(5).text());
+            String date = parent.child(5).text();
+            System.out.println("Дата: " + date);
+            if (date != null) {
+                System.out.println("LocaleDateTime: "
+                        + formatter.format(new SqlRuDateTimeParser().parse(date)));
+            }
+
         }
     }
 }
