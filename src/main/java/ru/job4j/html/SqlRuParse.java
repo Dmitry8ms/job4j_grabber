7248e593a161e83411422b14ade7417753bf3584
Dmitry8ms <dshatokhin@yandex.ru>
2021-11-15 13:56:58 +0700

    2.4. SqlRuParse [#285213 #228422]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,7 +4,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
@@ -12,22 +14,28 @@ import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
     public static void main(String[] args) throws Exception {
-        int depth = 5;
-        List<Post> posts = grabPostsFromSQLru(depth);
+        var parser = new SqlRuDateTimeParser();
+        SqlRuParse sqlRuParse = new SqlRuParse(parser);
+        String httpStr = "https://www.sql.ru/forum/job-offers/";
+        List<Post> posts = sqlRuParse.list(httpStr);
         for (var post : posts) {
             System.out.println(post);
         }
-
     }
 
-    private static List<Post> grabPostsFromSQLru(int depth) throws IOException {
+    @Override
+    public List<Post> list(String link) throws IOException {
         List<Post> posts = new LinkedList<>();
-        LocalDateTime localDateTime = null;
-        for (int i = 1; i <= depth; i++) {
-            String httpStr = "https://www.sql.ru/forum/job-offers/" + i;
-            Document doc = Jsoup.connect(httpStr).get();
+        for (int i = 1; i <= 5; i++) {
+            String sendLink = link + i;
+            Document doc = Jsoup.connect(sendLink).get();
             Elements rows = doc.select(".postslisttopic");
             for (Element row : rows) {
                 String vacancyLink = row.child(0).attr("href");
@@ -38,7 +46,8 @@ public class SqlRuParse {
         return posts;
     }
 
-    private static Post detail(String vacancyLink) throws IOException {
+    @Override
+    public Post detail(String vacancyLink) throws IOException {
         Post post = new Post();
         Document vacancyDoc = Jsoup.connect(vacancyLink).get();
         Elements elements = vacancyDoc.select(".msgTable");
@@ -46,7 +55,7 @@ public class SqlRuParse {
         title = title.replace(" [new]", "");
         String grabbedDate = elements.get(0).child(0).child(2).child(0).ownText();
         grabbedDate = grabbedDate.substring(0, grabbedDate.indexOf(" []"));
-        LocalDateTime created = new SqlRuDateTimeParser().parse(grabbedDate);
+        LocalDateTime created = dateTimeParser.parse(grabbedDate);
         String descr = elements.get(0).child(0).child(1).child(1).text();
         post.setTitle(title);
         post.setLink(vacancyLink);
