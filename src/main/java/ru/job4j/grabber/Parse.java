1ee82b1c7cdbff81ecfa5bf554053bba849cdc2f
Dmitry8ms <dshatokhin@yandex.ru>
2021-11-16 11:01:43 +0700

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359]

--- src/main/java/ru/job4j/grabber/Parse.java
+++ src/main/java/ru/job4j/grabber/Parse.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.util.List;
 
 public interface Parse {
+
     List<Post> list(String link) throws IOException;
 
     Post detail(String link) throws IOException;
+
 }
