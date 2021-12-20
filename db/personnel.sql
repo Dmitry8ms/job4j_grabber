aa620e1119e23052e69f8d4254034b7884ad2407
Dmitry8ms <dshatokhin@yandex.ru>
2021-12-20 17:16:22 +0700

    2. SQL Select by company [#2760]

---
+++ db/personnel.sql
@@ -0,0 +1,34 @@
+CREATE TABLE company
+(
+    id integer NOT NULL,
+    name character varying,
+    CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+CREATE TABLE person
+(
+    id integer NOT NULL,
+    name character varying,
+    company_id integer references company(id),
+    CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO company (id, name) VALUES (1, 'Yandex'), (2, 'Sber'), (3, 'Mail.ru'),
+(4, 'Inopolis'), (5, 'Telegram');
+
+INSERT INTO person (id, name, company_id) VALUES (1, 'Ivan', 1),
+(2, 'Petr', 1), (3, 'Dmitry', 2), (4, 'Alex', 2), (5, 'Stas', 3), (6, 'Vlad', 4), (7, 'Pavel', 5);
+
+SELECT * FROM company;
+SELECT * FROM person;
+
+SELECT p.name, c.name FROM person p JOIN company c ON p.company_id = c.id AND p.company_id <> 5;
+
+
+SELECT c.name company, count(p.id) n_personnel FROM person p  
+JOIN company c 
+ON p.company_id = c.id 
+GROUP BY c.name
+HAVING count(p.id) = (SELECT max(cnt) FROM (
+SELECT p.company_id, count (*) cnt FROM person p
+GROUP BY p.company_id) AS tbl1) AS tbl2;
+-- сам в шоке
